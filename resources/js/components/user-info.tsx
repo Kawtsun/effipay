e3982411d100a9ae7de43ff48f12df8e73efdbe0
Kawtsun <morpheusjoshua71@gmail.com>
2025-06-28 23:09:30 +0800

    fix login admin

--- resources/js/components/user-info.tsx
+++ resources/js/components/user-info.tsx
@@ -8,14 +8,14 @@ export function UserInfo({ user }: { user: User;}) {
     return (
         <>
             <Avatar className="h-8 w-8 overflow-hidden rounded-full">
-                <AvatarImage src={user.avatar} alt={user.username} />
+                <AvatarImage src="/img/setsu3.png" alt={user.username} />
                 <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
                     {getInitials(user.username)}
                 </AvatarFallback>
             </Avatar>
             <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{user.username}</span>
-                {<span className="truncate text-xs text-muted-foreground">{user.username}</span>}
+                {/* {<span className="truncate text-xs text-muted-foreground">{user.username}</span>} */}
             </div>
         </>
     );
