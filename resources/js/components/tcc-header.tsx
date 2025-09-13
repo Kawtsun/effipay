93cb5059fffb0ec8fc288f117d221ee7c343057d
Kawtsun <morpheusjoshua71@gmail.com>
2025-09-13 16:04:58 +0800

    add address in the header

--- resources/js/components/tcc-header.tsx
+++ resources/js/components/tcc-header.tsx
@@ -9,7 +9,10 @@ function TccHeader() {
         >
             <div className="flex items-center gap-4">
                 <img src="/img/tcc_logo2.jpg" alt="TCC Logo" className="h-15 w-15 object-contain bg-white rounded-full p-1" />
-                <span className="text-white text-lg font-semibold tracking-wide">Tomas Claudio Colleges</span>
+                <div className="flex flex-col">
+                    <span className="text-white text-lg font-semibold tracking-wide">Tomas Claudio Colleges</span>
+                    <span className="text-white text-xs font-normal tracking-wide opacity-80 mt-0.5">Taghangin, San Juan, Morong, Rizal</span>
+                </div>
             </div>
         </header>
     )
