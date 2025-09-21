649af12c3ed88308ceb0a291a9b970ada363651a
Kawtsun <morpheusjoshua71@gmail.com>
2025-09-21 08:08:27 +0800

    updated app header description

--- resources/js/components/tcc-header.tsx
+++ resources/js/components/tcc-header.tsx
@@ -3,15 +3,17 @@ function TccHeader() {
         <header
             className="fixed top-0 left-0 w-full z-40 flex items-center justify-start px-8"
             style={{
-                height: '84px',
+                height: '94px',
                 background: 'var(--primary)',
             }}
         >
             <div className="flex items-center gap-4">
                 <img src="/img/tcc_logo2.jpg" alt="TCC Logo" className="h-15 w-15 object-contain bg-white rounded-full p-1" />
                 <div className="flex flex-col">
-                    <span className="text-white text-lg font-semibold tracking-wide">Tomas Claudio Colleges</span>
-                    <span className="text-white text-xs font-normal tracking-wide opacity-80 mt-0.5">Taghangin, San Juan, Morong, Rizal</span>
+                    <span className="text-white text-xl font-bold tracking-wide text-center">Tomas Claudio Colleges</span>
+                    <span className="text-white text-xs font-normal tracking-wide opacity-80 mt-0.5 italic text-center">Higher Education Pioneer in Eastern Rizal</span>
+                    <span className="text-white text-xs font-normal tracking-wide opacity-80 mt-0.5 italic text-center">Taghangin, Morong, Rizal Philippines</span>
+                    {/* <span className="text-white text-xs font-normal tracking-wide opacity-80 mt-0.5 italic text-center">Tel. Nos.: (02) 234-5566</span> */}
                 </div>
             </div>
         </header>
