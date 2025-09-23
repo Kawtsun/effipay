4fd5797626af5d0b98faa6434927839e1724aba7
Kawtsun <morpheusjoshua71@gmail.com>
2025-09-24 07:48:05 +0800

    update employee seeder and remove uncessary migrations

--- database/migrations/2025_09_04_000001_update_salary_fields_to_decimal.php
+++ database/migrations/2025_09_04_000001_update_salary_fields_to_decimal.php
@@ -12,11 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('employees', function (Blueprint $table) {
-            $table->decimal('base_salary', 12, 2)->change();
-            $table->decimal('sss', 12, 2)->change();
-            $table->decimal('philhealth', 12, 2)->change();
-            $table->decimal('pag_ibig', 12, 2)->change();
-            $table->decimal('withholding_tax', 12, 2)->change();
+            $table->decimal('base_salary', 12, 2)->nullable()->change();
+            $table->decimal('sss', 12, 2)->nullable()->change();
+            $table->decimal('philhealth', 12, 2)->nullable()->change();
+            $table->decimal('pag_ibig', 12, 2)->nullable()->change();
+            $table->decimal('withholding_tax', 12, 2)->nullable()->change();
         });
         Schema::table('salaries', function (Blueprint $table) {
             $table->decimal('base_salary', 12, 2)->change();
@@ -33,11 +33,11 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('employees', function (Blueprint $table) {
-            $table->integer('base_salary')->change();
-            $table->integer('sss')->change();
-            $table->integer('philhealth')->change();
-            $table->integer('pag_ibig')->change();
-            $table->integer('withholding_tax')->change();
+            $table->integer('base_salary')->nullable()->change();
+            $table->integer('sss')->nullable()->change();
+            $table->integer('philhealth')->nullable()->change();
+            $table->integer('pag_ibig')->nullable()->change();
+            $table->integer('withholding_tax')->nullable()->change();
         });
         Schema::table('salaries', function (Blueprint $table) {
             $table->integer('base_salary')->change();
