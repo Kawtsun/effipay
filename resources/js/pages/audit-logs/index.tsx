11291619fe4364fba35aa82ab3d9a6f8dca19481
Kawtsun <morpheusjoshua71@gmail.com>
2025-09-27 08:10:04 +0800

    intro message

--- resources/js/pages/audit-logs/index.tsx
+++ resources/js/pages/audit-logs/index.tsx
@@ -2,6 +2,7 @@ import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import { ScrollText } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -10,11 +11,23 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-export default function AuditLogs() {
+export default function index() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Audit Logs" />
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
+            <div className="flex h-full flex-col gap-4 overflow-hidden py-6 px-2 sm:px-4 md:px-8">
+                {/* HEADER */}
+                <div className="flex-none">
+                    <h1 className="flex items-center gap-2 text-2xl font-semibold tracking-tight text-foreground">
+                        <ScrollText className='h-6 w-6 text-primary'/>
+                        Audit Logs
+                    </h1>
+                    <p className="text-sm text-muted-foreground">
+                        View system audit logs and track user actions for security and compliance.
+                    </p>
+                </div>
+
+                {/* MAIN CONTENT */}
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
