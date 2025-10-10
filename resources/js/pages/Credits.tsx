d844fc7ce4360f7c7bde1297f652d98d30c3afe6
Kawtsun <morpheusjoshua71@gmail.com>
2025-10-10 17:01:35 +0800

    kessuko band

--- resources/js/pages/Credits.tsx
+++ resources/js/pages/Credits.tsx
@@ -1,5 +1,6 @@
 // resources/js/Pages/Credits.tsx
 
+import { Card } from '@/components/ui/card';
 import { Item } from '@/components/ui/item';
 import AppLayout from '@/layouts/app-layout';
 import React from 'react';
@@ -9,10 +10,13 @@ export default function Credits() {
     return (
         // Your page content goes here
         <AppLayout>
-                <Item>
-                    <h1>Made by the Kessoku Band</h1>
-                    <img src="/img/kessoku band.jpg" alt="Kessoku Band" className="h-150 w-150 object-contain bg-white rounded-full p-1" />
-                </Item>
+            <Card className='shadow-none border-none text-center'>
+                <div className='pt-10'>
+                    <h1 className='text-5xl'><b>Credits</b></h1><br />
+                    <p className='text-3xl'>Made by the Kessoku Band</p><br />
+                    <img className='' src="img/Kessoku Band.jpg" alt="Kessoku Band" />
+                </div>
+            </Card>
         </AppLayout>
     );
 }
